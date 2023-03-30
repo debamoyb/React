31406e3d324f7a45f4e6870a7a70c1f700d74ddc
debamoyb <debamoyb@gmail.com>
2023-03-30 23:57:37 +0530

    Files Uploaded

---
+++ Homework/Session_12/product-card/src/App.jsx
@@ -0,0 +1,30 @@
+import './App.css';
+import { useState, useEffect } from 'react';
+import Card from './component/Card';
+
+const App = () => {
+
+const [state, setState] = useState([]);
+
+useEffect(() => {
+  fetch("https://fakestoreapi.com/products")
+  .then(res => res.json())
+  .then(data => setState(data));
+}, [])
+
+  return (
+    <>
+      <div className="container">
+        {
+          state.map((e, idx) => {
+            return (
+              <Card key={ e.id } title={ e.title } price={ e.price } image={ e.image } />
+            )
+          })  
+        }     
+      </div>
+    </>
+  )
+}
+
+export default App;
