3f63d2ba10caefd0fa6b64126983288a1f4e2d25
debamoyb <debamoyb@gmail.com>
2023-04-28 21:22:09 +0530

    Flies Uploaded

---
+++ Assignments/Class_component/netflix/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import Netflix from './components/Netflix';
+
+function App() {
+  return (
+    <div className="App">
+      <Netflix />
+    </div>
+  );
+}
+
+export default App;
